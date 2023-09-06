d9243762c155054a8618ef0f91b3e53a96ef5846
ewi josepha <ewijosepha@gmail.com>
2023-09-06 15:48:07 +0200

    adding functionality to buttons

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,34 +1,71 @@
- let listDisplay = document.getElementById('list-display')
- let checker = document.getElementById('checker')
-   
+let add = document.getElementById('add')
+let allitems=document.getElementById('list')
+let ul=document.querySelector('ul')
+let li=document.createElement('li')
+ul.append(li)
 
-function grocer  (e) {
-  e.preventDefault()
-  let typeList = document.getElementById('type-list').value
-  let newtype=typeList.toLowerCase()
-
-  if (newtype.length === " "){
-    console.log('enter text')
-    listDisplay.innerHTML="please enter text"
-   }else{
-    listDisplay.innerHTML=newtype
-    console.log(newtype)
-   }
-
-   newtype.split( ' ')
-   listDisplay.innerHTML=newtype.split( ' ')
-   console.log(newtype.split( ' '))
-   let pushlist=[]
-   for (let i=0; i<=newtype; i++) {
-  pushlist.push(i)
-  pushlist+='\n'
-
-  // listDisplay.innerHTML=pushlist
-
-   }
-
+let delett = document.getElementById('delete')
+function toDelete () {
+  // event.preventDefault()
+  allitems.value=""
+    li.innerHTML=""
+  
 }
 
+delett.addEventListener('click', ()=>{
+  toDelete()
+})
 
 
-checker.addEventListener('click', grocer)
+let inputt=document.querySelector('#input')
+
+inputt.addEventListener('input', function () {
+li.textContent= inputt.value
+if('input'=== inputt){
+  increment()
+}
+})
+
+
+
+function increment (e) {
+  let div= document.getElementById('carry')
+
+  e.preventDefault()
+  let h2= document.createElement('h2')
+
+h2.textContent= " " + inputt.value
+
+h2.addEventListener('click', ()=>{
+  h2.style.textDecoration= "line-through"
+})
+ div.insertAdjacentElement('beforeend', h2)
+
+ inputt.value=""
+ h2.value=""
+}
+
+add.addEventListener('click', increment)
+
+
+
+
+
+// function eventkey () {
+//   if(key='Enter') {
+//     addItems()
+//   }
+// }
+
+// function addItems () {
+//   li.innerHTML= input.value
+  
+//   }
+
+//   li.addEventListener('click', function () {
+//     li.style.textDecoration='line-through'
+
+//   allitems.innerHTML=('beforeend', li)
+// })
+
+
