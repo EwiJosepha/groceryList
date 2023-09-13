b1bca8807fc4335936f3a087335df30643feda6a
ewi josepha <ewijosepha@gmail.com>
2023-09-13 14:24:48 +0200

    functionality review

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,48 +1,33 @@
-let add = document.getElementById("add");
-let allitems = document.getElementById("list");
-let ul = document.querySelector("ul");
-// let button = document.createElement('button')
-let li = document.createElement("li");
-ul.append(li);
-// ul.append(button)
+let add = document.getElementById('add')
+let allitems = document.getElementById('list')
+let ul = document.querySelector('ul')
+let li = document.createElement('li')
+ul.append(li)
+let inputt = document.querySelector('#input')
 
-//
-
-// function btn () {
-//   li.innerHTML= " "
-//   allitems= ' '
-//   // h2= ' '
-//   console.log(li)
-//   // console.log(h2)
-// }
-
-// button.addEventListener('click', btn)
-
-let inputt = document.querySelector("#input");
-
-inputt.addEventListener("input", function () {
+inputt.addEventListener('input', function () {
   // li.textContent= inputt.value
-  if ("input" === inputt) {
-    increment(e);
+  if ('input' === inputt) {
+    increment(e)
   }
-});
+})
 
-let delett = document.getElementById("delete");
+let delett = document.getElementById('delete')
 function toDelete(event) {
-  event.preventDefault();
-  li.innerHTML = "";
+  event.preventDefault()
+  li.innerHTML = ''
 }
 
-delett.addEventListener("click", () => {
-  toDelete(event);
-});
+delett.addEventListener('click', () => {
+  toDelete(event)
+})
 
 function increment(e) {
-  e.preventDefault();
-  let div = document.getElementById("carry");
-  let h2 = document.createElement("h2");
-  let button = document.createElement("button");
-  h2.textContent = " " + inputt.value
+  e.preventDefault()
+  let div = document.getElementById('carry')
+  let h2 = document.createElement('h2')
+  let button = document.createElement('button')
+  h2.textContent = '' + inputt.value
   button.style.backgroundColor = 'red'
   button.style.width = '50px'
   button.style.height = '20px'
@@ -68,4 +53,4 @@ function increment(e) {
   h2.value = ''
 }
 
-add.addEventListener("click", increment);
+add.addEventListener("click", increment)
